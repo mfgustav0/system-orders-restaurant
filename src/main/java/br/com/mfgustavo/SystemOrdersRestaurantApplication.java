708239a90abd184d7a6fc467073a5ab6dc0a150b
mfgustav0 <gustavomonteirofavaro@gmail.com>
2025-01-21 02:19:08 -0300

    fix: format SystemOrdersRestaurantApplication

--- src/main/java/br/com/mfgustavo/SystemOrdersRestaurantApplication.java
+++ src/main/java/br/com/mfgustavo/SystemOrdersRestaurantApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SystemOrdersRestaurantApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SystemOrdersRestaurantApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SystemOrdersRestaurantApplication.class, args);
+    }
 }
