c11f93f5a72eba2d87bb7d403d3dc797a3fe1708
mfgustav0 <gustavomonteirofavaro@gmail.com>
2025-01-21 00:24:52 -0300

    first commit

---
+++ src/main/java/br/com/mfgustavo/SystemOrdersRestaurantApplication.java
@@ -0,0 +1,13 @@
+package br.com.mfgustavo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemOrdersRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemOrdersRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mfgustavo/SystemOrdersRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mfgustavo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemOrdersRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
